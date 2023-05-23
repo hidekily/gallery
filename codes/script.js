50724460f9282b385565eeedf249dcc13f68ad4e
hideki <74686890+hidekily@users.noreply.github.com>
2023-05-23 19:54:51 -0300

    Add files via upload

--- codes/script.js
+++ codes/script.js
@@ -81,27 +81,27 @@ let linhas = 0
 function linha(){
     switch(linhas){
         case 0:
-            document.getElementById('opt1').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/miatalinha.jpeg?v=1684640191569)' //1
-            document.getElementById('opt2').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/kuramalinha.jpeg?v=1684640189003)' //2
-            document.getElementById('opt3').style.backgroundImage = 'url(/codes/pics/)' //3
-            document.getElementById('opt4').style.backgroundImage = 'url(/codes/pics/)' //4
-            document.getElementById('opt5').style.backgroundImage = 'url(/codes/pics/)' //5
-            document.getElementById('opt6').style.backgroundImage = 'url(/codes/pics/)' //6
-            document.getElementById('opt7').style.backgroundImage = 'url(/codes/pics/)' //7
-            document.getElementById('opt8').style.backgroundImage = 'url(/codes/pics/)' //8
-            document.getElementById('opt9').style.backgroundImage = 'url(/codes/pics/)' //9
+            document.getElementById('opt1').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/miatalinha.jpeg?v=1684640191569)' //1 terços
+            document.getElementById('opt2').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/kuramalinha.jpeg?v=1684640189003)' //2 preencha
+            document.getElementById('opt3').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/linhasPlinha.jpeg?v=1684802705459)' //3 linhas
+            document.getElementById('opt4').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/diagonallinha.jpeg?v=1684882221090)' //4
+            document.getElementById('opt5').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/quadrolinha.jpeg?v=1684803037795)' //5 quadro
+            document.getElementById('opt6').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/olholinha.jpeg?v=1684803260189)' //6 olho
+            document.getElementById('opt7').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/padraolinha.jpeg?v=1684802711054)' //7 padrao
+            document.getElementById('opt8').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/contrastelinha.jpeg?v=1684803492034)' //8 contraste
+            
             linhas++
         break;
         case 1:
             document.getElementById('opt1').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/miata.jpeg?v=1684640190290)'//1
             document.getElementById('opt2').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/kurama.jpeg?v=1684640187225)'//2
-            document.getElementById('opt3').style.backgroundImage = 'url(/codes/pics/)'//3
-            document.getElementById('opt4').style.backgroundImage = 'url(/codes/pics/)'//4
-            document.getElementById('opt5').style.backgroundImage = 'url(/codes/pics/)'//5
-            document.getElementById('opt6').style.backgroundImage = 'url(/codes/pics/)'//6
-            document.getElementById('opt7').style.backgroundImage = 'url(/codes/pics/)'//7
-            document.getElementById('opt8').style.backgroundImage = 'url(/codes/pics/)'//8
-            document.getElementById('opt9').style.backgroundImage = 'url(/codes/pics/)'//9
+            document.getElementById('opt3').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/padrao.jpeg?v=1684802709357)'//3
+            document.getElementById('opt4').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/diagonal.jpeg?v=1684882220486)'//4
+            document.getElementById('opt5').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/quadro.jpeg?v=1684803037125)'//5
+            document.getElementById('opt6').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/olho.jpeg?v=1684803259634)'//6
+            document.getElementById('opt7').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/padrao.jpeg?v=1684802709357)'//7
+            document.getElementById('opt8').style.backgroundImage = 'url(https://cdn.glitch.global/4cc125e4-5188-4673-8e54-dac283e6c6a2/contraste.jpeg?v=1684803491125)'//8
+            
             linhas = 0
         break;
     }
